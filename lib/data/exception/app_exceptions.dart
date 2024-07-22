2341c3fad8e39555e66d18f6f38550a071c52f9e
rn-byte <ramthakur977+github@gmail.com>
2024-07-22 16:46:39 +0545

    app_exception class added

---
+++ lib/data/exception/app_exceptions.dart
@@ -0,0 +1,29 @@
+class AppExceptions implements Exception {
+  final _message;
+  final _prefix;
+
+  AppExceptions([this._message, this._prefix]);
+
+  @override
+  String toString() {
+    return '$_message $_prefix';
+  }
+}
+
+class NoInternetException extends AppExceptions {
+  NoInternetException([String? message])
+      : super(message, 'No Internet Connection');
+}
+
+class UnauthorizedException extends AppExceptions {
+  UnauthorizedException([String? message])
+      : super(message, 'Unauthorized access');
+}
+
+class RequestTimeout extends AppExceptions {
+  RequestTimeout([String? message]) : super(message, 'Request Timeout');
+}
+
+class FetchDataException extends AppExceptions {
+  FetchDataException([String? message]) : super(message, '');
+}
