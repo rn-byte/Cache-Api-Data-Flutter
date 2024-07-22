f93b7259a2e85fd947c00f6184fd775d431a4953
rn-byte <ramthakur977+github@gmail.com>
2024-07-22 17:14:52 +0545

    news_state class added

--- lib/bloc/news_state.dart
+++ lib/bloc/news_state.dart
@@ -1,8 +1,15 @@
+import 'package:cache_api_data_flutter/data/response/api_response.dart';
 import 'package:equatable/equatable.dart';
+import '../models/news_model.dart';
 
 class NewsState extends Equatable {
-  const NewsState();
+  final ApiResponse<NewsModel> newsList;
+  const NewsState({required this.newsList});
+
+  NewsState copyWith({ApiResponse<NewsModel>? newsList}) {
+    return NewsState(newsList: newsList ?? this.newsList);
+  }
 
   @override
-  List<Object?> get props => [];
+  List<Object?> get props => [newsList];
 }
